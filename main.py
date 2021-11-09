72d22c52fd1ac864c00993f406c7d795ad43b9b7
TheGreatSeb <42798013+TheGreatSeb@users.noreply.github.com>
2021-11-09 12:43:41 +0100

    Added wifi support to main

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from machine import Pin, TouchPad
 from time import ticks_ms, sleep_ms, time
+import wifiConnect
 import time
 import neopixel
 import lightAnimations
+lib = wifiConnect
 
 touch_pin_green = TouchPad(Pin(2))
 touch_pin_yellow = TouchPad(Pin(4))
@@ -11,15 +13,9 @@ touch_pin_red = TouchPad(Pin(12))
 while True:
     if (touch_pin_green.read() < 200):
         lightAnimations.greenCycle(0, 255, 0, 50)
-        lightAnimations.clear()
     elif (touch_pin_yellow.read() < 200):
         lightAnimations.yellowCycle(255, 155, 0, 50)
-        lightAnimations.clear()
     elif (touch_pin_red.read() < 200):
         lightAnimations.redCycle(255, 0, 0, 50)
-        lightAnimations.clear()
     else:
         lightAnimations.offline(2, 2, 2, 50)
-        lightAnimations.clear()
-
-
