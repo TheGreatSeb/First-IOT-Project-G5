049b8862825b315850d47707342fb193c348f49c
Jose6272 <92272492+Jose6272@users.noreply.github.com>
2021-11-10 11:56:13 +0100

    Update moist.py

--- Komponent kode/moist.py
+++ Komponent kode/moist.py
@@ -1,9 +1,18 @@
-from machine import Pin, ADC
-from time import sleep
-sensor = ADC(Pin(36))
-
-while True:
-    sensor_val = sensor.read()
-    print("it be moistn or wet",sensor_val)
-    sleep(0.1)
-    
+from machine import Pin, ADC
+
+from time import sleep
+
+sensor = ADC(Pin(39))
+
+
+
+while True:
+
+    sensor_val = sensor.read()
+
+    print("it be moistn or wet",sensor_val)
+
+    sleep(0.1)
+
+    
+
