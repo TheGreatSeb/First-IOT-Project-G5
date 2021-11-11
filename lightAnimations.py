b067d2afee1b2909bef4583721d711c2147ce4b1
TheGreatSeb <42798013+TheGreatSeb@users.noreply.github.com>
2021-11-11 09:22:05 +0100

    update again

--- lightAnimations.py
+++ lightAnimations.py
@@ -8,14 +8,23 @@ p = 15
 
 np = neopixel.NeoPixel(machine.Pin(p), n)
 
+#Clear LED's
 def clear():
   for i in range(n):
     np[i] = (0, 0, 0)
     np.write()
 
+#Color Cycle
+def colorCycle(color, bg, wait):
+  for i in range(2 * n):
+    for j in range(n):
+      np[i % n] = color
+      np.write()
+      time.sleep_ms(wait)
+
 # green cycle
 def greenCycle(r, g, b, wait):
-  for i in range(1 * n):
+  for i in range(2 * n):
     for j in range(n):
       np[j] = (0, 5, 0)
     np[i % n] = (r, g, b)
@@ -24,7 +33,7 @@ def greenCycle(r, g, b, wait):
 
 # yellow cycle
 def yellowCycle(r, g, b, wait):
-  for i in range(1 * n):
+  for i in range(2 * n):
     for j in range(n):
       np[j] = (3, 2, 0)
     np[i % n] = (r, g, b)
@@ -33,7 +42,7 @@ def yellowCycle(r, g, b, wait):
 
 # red cycle
 def redCycle(r, g, b, wait):
-  for i in range(1 * n):
+  for i in range(2 * n):
     for j in range(n):
       np[j] = (5, 0, 0)
     np[i % n] = (r, g, b)
@@ -42,9 +51,10 @@ def redCycle(r, g, b, wait):
 
 # offline status
 def offline(r, g, b, wait):
-  for i in range(1 * n):
+  for i in range(2 * n):
     for j in range(n):
       np[j] = (1, 1, 1)
     np[i % n] = (r, g, b)
     np.write()
     time.sleep_ms(wait)
+
--- main.py
+++ main.py
@@ -1,21 +1,39 @@
-from machine import Pin, TouchPad
-from time import ticks_ms, sleep_ms, time
-import wifiConnect
-import time
-import neopixel
+from machine import Pin
+from time import ticks_ms, sleep_ms
+#import wifiConnect
+import neopixel, dht
 import lightAnimations
-lib = wifiConnect
+#lib = wifiConnect
 
-touch_pin_green = TouchPad(Pin(2))
-touch_pin_yellow = TouchPad(Pin(4))
-touch_pin_red = TouchPad(Pin(12))
+dht11_interval = 2000
+dht11_state = 0
+dht11_previousTime = 0
+sensor = dht.DHT11(Pin(26))
 
 while True:
-    if (touch_pin_green.read() < 200):
-        lightAnimations.greenCycle(0, 255, 0, 50)
+    current_time = ticks_ms()
+    if (current_time - dht11_previousTime > dht11_interval):
+        dht11_previousTime = current_time
+        sensor.measure()
+        temp = sensor.temperature()
+        hum = sensor.humidity()
+        print("temperature: %3.1f C" % temp)
+        print("Hum: %3.1f" %  hum)
+        if (temp < 25 and hum < 60):
+            lightAnimations.clear
+            print("Lys: Clear")
+        elif (temp < 30 and hum < 70):
+            lightAnimations.yellowCycle(255, 155, 0, 50)
+            print("Lys: Yellow")
+        elif (temp < 40 and hum < 100):
+            lightAnimations.redCycle(255, 0, 0, 50)
+            print("Lys: Red")
+    """
     elif (touch_pin_yellow.read() < 200):
         lightAnimations.yellowCycle(255, 155, 0, 50)
     elif (touch_pin_red.read() < 200):
         lightAnimations.redCycle(255, 0, 0, 50)
     else:
         lightAnimations.offline(2, 2, 2, 50)
+        """
+
