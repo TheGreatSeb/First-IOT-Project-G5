cc336ba4e1fefc8bd7be3be16006116111e3ab45
Jose6272 <92272492+Jose6272@users.noreply.github.com>
2021-11-12 13:39:16 +0100

    Update buzzer.py

--- Komponent kode/buzzer.py
+++ Komponent kode/buzzer.py
@@ -1,6 +1,8 @@
 from machine import Pin, PWM
 import time
 
-buzzer = PWM(Pin(14), freq=10, duty=5)
-time.sleep(70)
-buzzer.deinit()
+def buzzOn(x):
+    buzzer = PWM(Pin(14), freq=10, duty=5)
+    time.sleep(x)
+    buzzer.duty(0)
+    time.sleep(x)
