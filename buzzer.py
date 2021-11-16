95cdc31e29a4cb278324d13743eda3fbd24a93de
TheGreatSeb <42798013+TheGreatSeb@users.noreply.github.com>
2021-11-16 09:40:55 +0100

    blah

--- buzzer.py
+++ buzzer.py
@@ -1,11 +1,8 @@
 from machine import Pin
-import time
 
-buzbuz = Pin(14, Pin.OUT)
+buzbuz = (Pin(14), Pin.OUT)
 
 def set_buzbuz_on():
     buzbuz.value(1)
-    print("BUZZ ONNN")
 def set_buzbuz_off():
     buzbuz.value(0)
-
