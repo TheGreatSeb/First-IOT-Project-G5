d306f70ceffc8c0f854bfbff408569f4e9316d8e
TheGreatSeb <42798013+TheGreatSeb@users.noreply.github.com>
2021-11-24 09:05:14 +0100

    Final update

--- GPS filer/credentials.py
+++
@@ -1,9 +0,0 @@
-credentials = {
-    'ssid' : 'anas',
-    'password' : '12345678',
-    'ADAFRUIT_IO_URL' : b'io.adafruit.com',
-    'ADAFRUIT_USERNAME' : b'Anas0418',
-    'ADAFRUIT_IO_KEY' : b'aio_ccvm39G1enGpTlMqNF3jiStHpR9x',
-    'ADAFRUIT_IO_PUB_FEEDNAME' : b'bot_pub',
-    'ADAFRUIT_IO_SUB_FEEDNAME' : b'bot_sub'
-    }
--- Komponent kode/buzzer.py
+++
@@ -1,8 +0,0 @@
-from machine import Pin, PWM
-import time
-
-def buzzOn(x):
-    buzzer = PWM(Pin(14), freq=10, duty=5)
-    time.sleep(x)
-    buzzer.duty(0)
-    time.sleep(x)
